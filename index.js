351b3a5379afb4f5c3577a9934851c77dcfc0484
1DaNa1 <144629312+1DaNa1@users.noreply.github.com>
2024-04-01 10:40:41 +0300

    Оновлено

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let menuBtn = document.querySelector('.menu-btn');
-    let menu = document.querySelector('.menu');
-    
-    menuBtn.addEventListener('click', function() {
-        menuBtn.classList.toggle('active');
-        menu.classList.toggle('active');
-    });
+// Отримує посилання на елементи меню
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+// Додає обробник події для бургер-меню
+menuBtn.addEventListener('click', () => {
+  // Перемикає клас 'active' для бургер-меню та меню
+  menuBtn.classList.toggle('active');
+  menu.classList.toggle('active');
 });
